97b0c1f382cb8c299eacc125ab6366dc395e705c
OmarDarwish7 <omartdarwish@gmail.com>
2023-06-05 01:00:27 +0300

    middleware commit

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://kermesse-e2b68.web.app',
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://kermesse-e2b68.web.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
   await app.listen(3000);
 }
